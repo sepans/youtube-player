b3f6cab1a289694141569599a6746a9c69ffe7c2
Sepand Ansari <sepans@sepans.com>
2016-05-09 16:40:49 -0400

    styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -34,7 +34,7 @@ class AppComponent extends React.Component {
 
   render() {
     return (
-      <div className="index">
+      <div className='index container'>
         {/*<img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>*/}
         <TagPlayer youtubeId={this.topId} conceptData={this.conceptData}></TagPlayer>
