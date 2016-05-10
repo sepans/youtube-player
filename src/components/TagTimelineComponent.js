16f50936e306fadcc943fc7b9b7c13725f7861e0
Sepand Ansari <sepans@sepans.com>
2016-05-09 20:01:57 -0400

    thumbnails

--- src/components/TagTimelineComponent.js
+++ src/components/TagTimelineComponent.js
@@ -2,7 +2,7 @@
 'use strict';
 
 import React from 'react';
-//import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom'
 
 require('styles//TagPlayer.css');
 
@@ -22,26 +22,85 @@ class TagTimelineComponent extends React.Component {
 	}
 
 	componentWillReceiveProps(newProps) {
-		console.log('UPDATE', this.props, newProps)
+		//console.log('UPDATE', this.props, newProps)
 		if(newProps.videoURL) {
-			console.log('VIDEO URL', newProps.videoURL);
+			//console.log('VIDEO URL', newProps.videoURL);
+			console.log(this.refs);
+			const vid= document.createElement('video');
+			//vid.src = newProps.videoURL;
+			const self = this;
+				console.log('onloadeddata', vid);
+
+				//self.props.conceptData.forEach(function(d,i) {
+					
+
+					let createScreenshot = function(i) {
+						console.log('createScreenshot', i)
+						let d = self.props.conceptData[i];
+
+						const canvas =  ReactDOM.findDOMNode( self.refs['canvas'+i])
+
+						// vid.src is the same. shouldn't change but without reseting it
+						// inloadeddata is not getting called (which seems to be needed)
+						vid.src = newProps.videoURL;
+	    				vid.currentTime = d.start;
+
+	    				const ctx = canvas.getContext('2d');
+
+
+	    				console.log('waiting for load');
+				   
+
+						vid.onloadeddata = function() {
+							console.log('DRAW', vid.currentTime, i,  self.props.conceptData.length)
+							  
+							ctx.drawImage(vid, 0, 0, canvas.width, canvas.height);
+							if(i<self.props.conceptData.length-1) {
+								console.log('call', i+1);
+								createScreenshot(i+1)
+							}
+						}
+
+					}
+
+					createScreenshot(0);
+					
+				    
+
+				    
+
+    				//ctx.restore();
+
+
+				   
+				//});
+
+			//}
+
 		}
 	}
 
 	render() {
 		//const width = 500;
 		let conceptList = this.props.conceptData.map((d, i) => {
+
+			const styles = {
+							'left' :  Math.round(d.start)+'px',
+							 'width' : (d.end-d.start)+'px'
+							}
 			
 			return (
-		 		<li key={i}  onClick={this._gotoFrame.bind(this, i)} style={{left: d.start+'px'}}>
-		 			<span className='info'>
-		 				<span className='time'>{Math.round(d.start)}s</span>
-		 				<span className='tags'>{d.concepts}</span>
-		 				<canvas className='screenshot'></canvas>
+				
+			 		<li key={i}  onClick={this._gotoFrame.bind(this, i)} style={Object.assign(styles)}>
+			 			<span className='info'>
+			 				<span className='time'>{Math.round(d.start)}s</span>
+			 				<span className='tags'>{d.concepts}</span>
+			 				<canvas className='screenshot' ref={'canvas'+i}></canvas>
 
-		 			</span>
+			 			</span>
 
-		 		</li>
+			 		</li>
+			 	
 		 	)
 		})
 
@@ -50,6 +109,8 @@ class TagTimelineComponent extends React.Component {
 				<ul>
 					{conceptList}
 				</ul>
+				
+
 			</div>
 		);
 	}
